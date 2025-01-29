bb00da77fa74106e67ada8fbf0a8bb9be300e7b1
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-01-29 10:35:42 +0530

    middleware

--- src/Middleware/middleware.ts
+++ src/Middleware/middleware.ts
@@ -1,24 +1,25 @@
 import e, { Request, Response, NextFunction } from "express";
 import { isCelebrateError } from "celebrate";
-import { sendResponse } from "../Utility/Responsehelper";
+import { sendErrorResponse } from "../Utility/Responsehelper";
 
 export const requestLogger = (request: Request, resposne: Response, next: NextFunction) => {
-  console.log("API URL =>", request.url, 
-              " Body =>", request.body, 
-              " Parameter =>", request.params)
+  console.log("API URL =>", request.url,
+    " Method =>", request.method,
+    " Body =>", request.body,
+    " Parameter =>", request.params)
   next()
 }
 
 export const errorHandler = (error: any, request: Request, resposne: Response, next: NextFunction) => {
-  console.log("API Error Handling =>", 
-              error.message, 
-              error.stack);
+  console.log("<<<=== Global Error Message ===>>>");
+  console.log("Message =>", error.message);
+  console.log("Error Stack =>", error.stack);
   console.error(error);
 
   const statusCode = 500; // Default to 500 if no status code is set
   const errorMessage = error.message || "Internal Server Error"; // Default to a generic error message
 
-  sendResponse(resposne, statusCode, null, "", errorMessage);
+  sendErrorResponse(resposne, statusCode, errorMessage);
   next()
 }
 
@@ -35,8 +36,8 @@ export const celebrateErrorHandler = (
 
     const details = errorBody || errorParams || errorQuery;
 
-    const errorMessage = details?.details[0]?.message
-    sendResponse(res, 400, null, "", errorMessage);
+    const errorMessage = details?.details[0]?.message || "";
+    sendErrorResponse(res, 400, errorMessage);
   } else {
     next(err); // Pass other errors to the default error handler
   }
